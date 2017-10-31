a3a293d5e2002a3ef22c4d251a57499f5f4ed26b
zao1214 <hexa1214@gmail.com>
2017-10-31 19:19:24 +0900

    renamed PositionReporter -> PositionReport

--- Source/EscapeRoom/PositionReport.cpp
+++ Source/EscapeRoom/PositionReport.cpp
@@ -1,31 +1,29 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Zao 2017
 
-#include "PositionReporter.h"
+#include "PositionReport.h"
 
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!!"));
+
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
--- Source/EscapeRoom/PositionReport.h
+++ Source/EscapeRoom/PositionReport.h
@@ -1,20 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Zao 2017
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "PositionReporter.generated.h"
+#include "PositionReport.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class ESCAPEROOM_API UPositionReporter : public UActorComponent
+class ESCAPEROOM_API UPositionReport : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UPositionReporter();
+	UPositionReport();
 
 protected:
 	// Called when the game starts
