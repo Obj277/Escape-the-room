286690b3afed0fc0a81ebfc33e518ae548913164
zao1214 <hexa1214@gmail.com>
2017-11-09 16:53:28 +0900

    Refactored FDoorEvent and Converted UE version 4.17 to 4.18

--- Source/EscapeRoom/OpenDoor.h
+++ Source/EscapeRoom/OpenDoor.h
@@ -6,7 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ESCAPEROOM_API UOpenDoor : public UActorComponent
@@ -17,12 +17,6 @@ public:
 	// Sets default values for this component's properties
 	UOpenDoor();
 
-	void OpenDoor();
-	void CloseDoor();
-
-	UPROPERTY(BlueprintAssignable)
-	FOnOpenRequest OnOpenRequest;
-
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -31,17 +25,18 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	UPROPERTY(BlueprintAssignable)
+		FDoorEvent OnOpen;
+
+	UPROPERTY(BlueprintAssignable)
+		FDoorEvent OnClose;
+
 private:
-	UPROPERTY(EditAnywhere)
-	float OpenAngle = -90.0f;
-	
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate = nullptr;
 
 	UPROPERTY(EditAnywhere)
-	float DoorCloseDelay = 1.f;
-
-	float LastDoorOpenTime;
+	float TriggerMass = 30.f;
 
 	AActor* Owner = nullptr;
 
